7ff4e9b7af7e89e54c80c126bef1a1557ed5e8dc
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-11-08 16:35:36 -0800

    Apply template

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,4 +9,22 @@ package 'logrotate' do
     action :upgrade
 end
 
+# Exit recipe if not logrotate entry attributes defined
+unless node['cas_logrotate']['entries']
+    return
+end
 
+# Configure logrotate entries
+node['cas_logrotate']['entries'].each do |entry, data|
+    template entry do
+        path "/etc/logrotate.d/#{entry}"
+        source 'logrotate.erb'
+        owner 'root'
+        group 'root'
+        mode 0644
+        action :create
+        variables ({
+            data: data
+        })
+    end
+end
